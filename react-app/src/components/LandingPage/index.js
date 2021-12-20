15c2b552d441fcd5e3ff172c688b65fca5ec2ecd
aivnerrad <aivnerrad1993@gmail.com>
2021-12-19 21:00:27 -0500

    done for today

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import "./LandingPage.css"
 
 const LandingPage = () => {
   return (
-    <div>
+    <div id="landing-page">
       <h1>Landing Page</h1>
+      <div id="landing-image" style={{"background-image" : "url('./images/home.jpg')"}}></div>
       <NavLink to="/listings">Listings Page</NavLink>
     </div>
   );
