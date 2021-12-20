af93fd0c706e8811e7f06cb6b718b842517e5510
aivnerrad <aivnerrad1993@gmail.com>
2021-12-20 17:14:57 -0500

    break time

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -4,10 +4,19 @@ import "./LandingPage.css"
 
 const LandingPage = () => {
   return (
-    <div id="landing-page">
-      <h1>Landing Page</h1>
-      <div id="landing-image" style={{"background-image" : "url('./images/home.jpg')"}}></div>
-      <NavLink to="/listings">Listings Page</NavLink>
+    <div className='black'>
+        <div id="landing-page">
+        <h1>Landing Page</h1>
+        <div id="landing-image" style={{"background-image" : "url(/images/home.jpg)"}}>
+          <span id="landing-image-text">Not sure where your next home is? Perfect.</span>
+          <div id="landing-image-button">
+            <NavLink to="/listings" ><span id="landing-image-button-text">Take me home!</span></NavLink>
+          </div>
+        </div>
+      </div>
+      <div className='white'>
+
+      </div>
     </div>
   );
 }
